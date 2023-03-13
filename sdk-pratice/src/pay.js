bedc293f2a81908668e9db0673b14223959286d4
rooney <bch3454@gmail.com>
2023-03-13 15:12:33 +0900

    Study: SDK 테스트

---
+++ sdk-pratice/src/pay.js
@@ -0,0 +1,19 @@
+class Pay {
+  constructor() {
+    console.log("constructor");
+  }
+
+  create() {
+    console.log("create");
+  }
+
+  open() {
+    console.log("open");
+  }
+
+  getVersion() {
+    console.log("v.1.0.0");
+  }
+}
+
+export default Pay;
